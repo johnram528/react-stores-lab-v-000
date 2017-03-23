f8a5385e61d408212709ede40414f65757cb66ba
johnram528 <johnram528@gmail.com>
2017-03-22 23:34:17 -0400

    Done.

--- components/App.js
+++ components/App.js
@@ -1,27 +1,46 @@
 const React = require('react');
+const actions = require('../actions/index')
+const counterStore = require('../stores/counterStore');
 
 class App extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
+      counter: counterStore.getState()
       // Your implementation here.
     };
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
   }
   componentDidMount () {
-    // Your implementation here.
+    this.removeListener = counterStore.addListener((counter) => {
+      this.setState({counter})
+    })// Your implementation here.
   }
   componentWillUnmount () {
-    // Your implementation here.
+    this.removeListener();// Your implementation here.
   }
+
+  handleIncrement(event) {
+    event.preventDefault();
+    actions.increment();
+  }
+
+  handleDecrement(event) {
+    event.preventDefault();
+    actions.decrement();
+  }
+
   render () {
     return (
       <div className='app'>
-        <h1 className='counter'></h1>
+        <h1 className='counter'>{this.state.counter}</h1>
         <div className='actions'>
-          <button className='decrement'>
+          <button className='decrement' onClick={this.handleDecrement}>
             -
           </button>
-          <button className='increment'>
+          <button className='increment' onClick={this.handleIncrement}>
             +
           </button>
         </div>
--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,4 +1,19 @@
-class CounterStore {
+const Store = require('./Store');
+
+class CounterStore extends Store {
+  constructor() {
+    super(0);
+  }
+
+  increment() {
+    const newState = this.getState() + 1
+    this.setState(newState);
+  }
+
+  decrement() {
+    const newState = this.getState() - 1
+    this.setState(newState);
+  }
   // Your implementation here.
   // Hint: extend the Store class!
 }
